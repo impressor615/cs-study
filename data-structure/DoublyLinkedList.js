7c384e44379c8cfc71ee670b5cbc6ae89ef5cbfb
seonghyeon <hyeon@apartmentary.com>
2019-03-08 00:11:46 +0900

    add DoublyLinkedList and push method
    [Why]
    
    *

---
+++ data-structure/DoublyLinkedList.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
+    return this;
+  }
+}
+
+
+const linkedList = new DoublyLinkedList();
+linkedList
+  .push('10')
+  .push('20')
+  .push('30')
+
+
+console.log(linkedList);
+
