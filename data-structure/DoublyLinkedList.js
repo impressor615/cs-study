07cee1de985d131c57f709c4c3568614d95cdf43
seonghyeon <hyeon@apartmentary.com>
2019-03-12 23:30:16 +0900

    add DoublyLinkedList pop method
    [Why]
    
    *

--- data-structure/DoublyLinkedList.js
+++ data-structure/DoublyLinkedList.js
@@ -15,27 +15,45 @@ class DoublyLinkedList {
 
   push(val) {
     const newNode = new Node(val);
+    this.length += 1;
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
+      return this;
     }
 
-    this.length += 1;
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
     return this;
+  }
+
+  pop() {
+    if (!this.head) return;
+    const poppedNode = this.tail;
+
+    this.length -= 1;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      return poppedNode;
+    }
+
+    this.tail = poppedNode.prev;
+    this.tail.next = null;
+    poppedNode.prev = null;
+    return poppedNode;
   }
 }
 
 
-const linkedList = new DoublyLinkedList();
-linkedList
+const doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList
   .push('10')
   .push('20')
   .push('30')
+  .pop()
 
 
-console.log(linkedList);
+console.log(doublyLinkedList);
 
