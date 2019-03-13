5198a4cf152a1456e6defe998b2088e8bfd5cc2c
seonghyeon <hyeon@apartmentary.com>
2019-03-14 00:55:40 +0900

    add BinarySearchTree insert method
    [Why]
    
    *

---
+++ data-structure/BinarySearchTree.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while (true) {
+      if (current.value === value) return;
+      if (current.value > value) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+
+const binarySearchTree = new BinarySearchTree()
+  .insert(10)
+  .insert(20)
+  .insert(5)
+  .insert(7)
+
+console.log(binarySearchTree);
